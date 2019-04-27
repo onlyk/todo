4f6cbacebf395d4d3edb5849e8088b2344441699
Skurt Arseniy <skurtars@gmail.com>
2019-04-27 19:53:28 +0300

    eto rabotaet

--- public/index.php
+++ public/index.php
@@ -1,5 +1,46 @@
 <?php
 
+namespace todo;
 require __DIR__ . '/../vendor/autoload.php';
 
-echo 'lol';
+$opt = array(
+	\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION
+);
+
+$pdo = new \PDO('pgsql:host=localhost;port=5432;dbname=todo;', "postgres", "misamisa", $opt);
+
+// $result = $pdo->query("SELECT * FROM tasks")->fetchAll(\PDO::FETCH_ASSOC);
+
+
+
+switch ($_SERVER['PATH_INFO']) {
+	case '':
+		$task = new Task($pdo);
+		$task->returnAllTasks($pdo);
+		break;
+	case '/task/create':
+		$name = $_GET['name'];
+		$body = $_GET['body'];
+
+		$task = new Task($pdo);
+		$task->createTask($name, $body);
+		break;
+	case '/task/markdone':
+		$id = $_GET['id'];
+		$task = new Task($pdo);
+		$task->markDone($id);
+		break;
+	case '/task/delete':
+		$id = $_GET['id'];
+		$task = new Task($pdo);
+		$task->deleteTask($id);
+		break;
+	default:
+		echo 404;
+		break;
+}
+
+
+// $stmt = $pdo->prepare('Some sql here');
+// $stmt->execute(['someParam1' => 13, 'someParam2' => 42]);
+// $result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
--- src/Task.php
+++ src/Task.php
@@ -3,8 +3,50 @@
 
 namespace todo;
 
-
 class Task
 {
+	private $pdo;
+	public function __construct($pdo)
+	{
+		$this->pdo = $pdo;
+	}
+	public function getNextId()
+	{
+		$stmt = $this->pdo->prepare('SELECT id FROM tasks WHERE id=(SELECT MAX(id) FROM tasks)');
+		$stmt->execute();
+		$result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+		return ($result[0]['id'] ?? 0) + 1;
+	}
 
+	public function createTask($name, $body)
+	{
+		$id = $this->getNextId();
+		$stmt = $this->pdo->prepare("INSERT INTO tasks (name, id, body, done) VALUES (:name, :id, :body, 0)");
+		$stmt->execute([':name' => $name, ':id' => $id, ':body' => $body]);
+	}
+
+	public function returnAllTasks()
+	{
+		$stmt = $this->pdo->prepare("SELECT * FROM tasks");
+		$stmt->execute();
+		$result = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+		foreach ($result as $task) {
+			echo $task['name'] . '—' . $task['body'] . '<br>';
+		}
+
+	}
+
+	public function markDone($id)
+	{
+		$stmt = $this->pdo->prepare("UPDATE tasks SET done = 1 
+	   WHERE id = :id");
+
+		$stmt->execute([':id' => $id]);
+	}
+
+	public function deleteTask($id)
+	{
+		$stmt = $this->pdo->prepare("DELETE FROM tasks WHERE id = :id");
+		$stmt->execute([':id' => $id]);
+	}
 }
